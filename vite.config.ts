c783f50261e95fd20fd49085a1d9ec26ce3f07aa
Geniuh <geniuh1@gmail.com>
2024-09-15 15:33:21 -0500

    configuracion de vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-  }
-})
-
+    outDir: 'dist', // Directorio de salida para la build
+    rollupOptions: {
+      input: {
+        main: 'index.html', // Archivo de entrada principal
+      }
+    }
+  },
+  server: {
+    port: 3000, // Puerto para el servidor de desarrollo
+  },
+  // Puedes agregar otras configuraciones según lo necesites
+});
